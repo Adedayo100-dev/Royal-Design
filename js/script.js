319a6cc1b35152959163be20fa9b29dd0e536cdd
Taiwo Adedayo {{ Dee - Why }} <66147046+Adedayo100-ux@users.noreply.github.com>
2022-01-16 23:13:20 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+// When page is scrolled
+// window.onscroll = function() {scrollFunction()};
+
+// var nav = document.getElementById("nav");
+// function scrollFunction() {
+//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    
+//   } else {
+
+//   }
+// }
